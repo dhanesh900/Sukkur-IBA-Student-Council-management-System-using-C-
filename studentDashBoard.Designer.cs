b674dba5afa3ce18af6bc31934e27ecf8498d8ab
Dhanesh kumar <dhanesh.bcss19@iba-suk.edu.pk>
2022-03-28 16:05:55 +0500

    creating Admin dashboard for managing student records

---
+++ studentDashBoard.Designer.cs
@@ -0,0 +1,326 @@
+﻿
+namespace Sisc
+{
+    partial class studentDashBoard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.guna2CirclePictureBox2 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.btnConfirm = new Guna.UI2.WinForms.Guna2Button();
+            this.btnCancel = new Guna.UI2.WinForms.Guna2Button();
+            this.pParty1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblparty1 = new System.Windows.Forms.Label();
+            this.pbParty1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.lblparty2 = new System.Windows.Forms.Label();
+            this.pbParty2 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.btnp1 = new Guna.UI2.WinForms.Guna2RadioButton();
+            this.btnp2 = new Guna.UI2.WinForms.Guna2RadioButton();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).BeginInit();
+            this.pParty1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbParty1)).BeginInit();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbParty2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(46)))), ((int)(((byte)(146)))));
+            this.panel1.Controls.Add(this.lblTitle);
+            this.panel1.Controls.Add(this.guna2CirclePictureBox2);
+            this.panel1.Controls.Add(this.guna2CirclePictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(854, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(332, 41);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(177, 30);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "Vote Casting";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // guna2CirclePictureBox2
+            // 
+            this.guna2CirclePictureBox2.Image = global::Sisc.Properties.Resources.siscIcon;
+            this.guna2CirclePictureBox2.ImageRotate = 0F;
+            this.guna2CirclePictureBox2.Location = new System.Drawing.Point(754, 21);
+            this.guna2CirclePictureBox2.Name = "guna2CirclePictureBox2";
+            this.guna2CirclePictureBox2.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CirclePictureBox2.ShadowDecoration.Parent = this.guna2CirclePictureBox2;
+            this.guna2CirclePictureBox2.Size = new System.Drawing.Size(64, 64);
+            this.guna2CirclePictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2CirclePictureBox2.TabIndex = 1;
+            this.guna2CirclePictureBox2.TabStop = false;
+            // 
+            // guna2CirclePictureBox1
+            // 
+            this.guna2CirclePictureBox1.Image = global::Sisc.Properties.Resources.siscIcon;
+            this.guna2CirclePictureBox1.ImageRotate = 0F;
+            this.guna2CirclePictureBox1.Location = new System.Drawing.Point(24, 21);
+            this.guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
+            this.guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CirclePictureBox1.ShadowDecoration.Parent = this.guna2CirclePictureBox1;
+            this.guna2CirclePictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.guna2CirclePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2CirclePictureBox1.TabIndex = 0;
+            this.guna2CirclePictureBox1.TabStop = false;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.BorderRadius = 20;
+            this.btnConfirm.CheckedState.Parent = this.btnConfirm;
+            this.btnConfirm.CustomImages.Parent = this.btnConfirm;
+            this.btnConfirm.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnConfirm.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnConfirm.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnConfirm.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnConfirm.DisabledState.Parent = this.btnConfirm;
+            this.btnConfirm.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(46)))), ((int)(((byte)(146)))));
+            this.btnConfirm.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirm.ForeColor = System.Drawing.Color.White;
+            this.btnConfirm.HoverState.Parent = this.btnConfirm;
+            this.btnConfirm.Location = new System.Drawing.Point(710, 550);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.ShadowDecoration.Parent = this.btnConfirm;
+            this.btnConfirm.Size = new System.Drawing.Size(94, 34);
+            this.btnConfirm.TabIndex = 1;
+            this.btnConfirm.Text = "Confirm";
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BorderRadius = 20;
+            this.btnCancel.CheckedState.Parent = this.btnCancel;
+            this.btnCancel.CustomImages.Parent = this.btnCancel;
+            this.btnCancel.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnCancel.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnCancel.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnCancel.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnCancel.DisabledState.Parent = this.btnCancel;
+            this.btnCancel.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(46)))), ((int)(((byte)(146)))));
+            this.btnCancel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.HoverState.Parent = this.btnCancel;
+            this.btnCancel.Location = new System.Drawing.Point(596, 550);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.ShadowDecoration.Parent = this.btnCancel;
+            this.btnCancel.Size = new System.Drawing.Size(94, 34);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // pParty1
+            // 
+            this.pParty1.BackColor = System.Drawing.Color.Black;
+            this.pParty1.Controls.Add(this.panel2);
+            this.pParty1.Location = new System.Drawing.Point(58, 164);
+            this.pParty1.Name = "pParty1";
+            this.pParty1.Size = new System.Drawing.Size(266, 264);
+            this.pParty1.TabIndex = 3;
+            this.pParty1.Paint += new System.Windows.Forms.PaintEventHandler(this.pParty1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(46)))), ((int)(((byte)(148)))));
+            this.panel2.Controls.Add(this.lblparty1);
+            this.panel2.Controls.Add(this.pbParty1);
+            this.panel2.Location = new System.Drawing.Point(8, 8);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(245, 244);
+            this.panel2.TabIndex = 4;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // lblparty1
+            // 
+            this.lblparty1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblparty1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblparty1.ForeColor = System.Drawing.Color.White;
+            this.lblparty1.Location = new System.Drawing.Point(32, 203);
+            this.lblparty1.Name = "lblparty1";
+            this.lblparty1.Size = new System.Drawing.Size(171, 23);
+            this.lblparty1.TabIndex = 1;
+            this.lblparty1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pbParty1
+            // 
+            this.pbParty1.ImageRotate = 0F;
+            this.pbParty1.Location = new System.Drawing.Point(32, 28);
+            this.pbParty1.Name = "pbParty1";
+            this.pbParty1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.pbParty1.ShadowDecoration.Parent = this.pbParty1;
+            this.pbParty1.Size = new System.Drawing.Size(171, 172);
+            this.pbParty1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbParty1.TabIndex = 0;
+            this.pbParty1.TabStop = false;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Black;
+            this.panel5.Controls.Add(this.panel6);
+            this.panel5.Location = new System.Drawing.Point(493, 164);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(266, 264);
+            this.panel5.TabIndex = 6;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(46)))), ((int)(((byte)(148)))));
+            this.panel6.Controls.Add(this.lblparty2);
+            this.panel6.Controls.Add(this.pbParty2);
+            this.panel6.Location = new System.Drawing.Point(8, 8);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(245, 244);
+            this.panel6.TabIndex = 4;
+            // 
+            // lblparty2
+            // 
+            this.lblparty2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblparty2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblparty2.ForeColor = System.Drawing.Color.White;
+            this.lblparty2.Location = new System.Drawing.Point(34, 203);
+            this.lblparty2.Name = "lblparty2";
+            this.lblparty2.Size = new System.Drawing.Size(171, 23);
+            this.lblparty2.TabIndex = 1;
+            this.lblparty2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pbParty2
+            // 
+            this.pbParty2.ImageRotate = 0F;
+            this.pbParty2.Location = new System.Drawing.Point(34, 28);
+            this.pbParty2.Name = "pbParty2";
+            this.pbParty2.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.pbParty2.ShadowDecoration.Parent = this.pbParty2;
+            this.pbParty2.Size = new System.Drawing.Size(171, 172);
+            this.pbParty2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbParty2.TabIndex = 0;
+            this.pbParty2.TabStop = false;
+            // 
+            // btnp1
+            // 
+            this.btnp1.AutoSize = true;
+            this.btnp1.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.btnp1.CheckedState.BorderThickness = 0;
+            this.btnp1.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.btnp1.CheckedState.InnerColor = System.Drawing.Color.White;
+            this.btnp1.CheckedState.InnerOffset = -4;
+            this.btnp1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnp1.Location = new System.Drawing.Point(140, 447);
+            this.btnp1.Name = "btnp1";
+            this.btnp1.Size = new System.Drawing.Size(77, 29);
+            this.btnp1.TabIndex = 9;
+            this.btnp1.Text = "Vote";
+            this.btnp1.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.btnp1.UncheckedState.BorderThickness = 2;
+            this.btnp1.UncheckedState.FillColor = System.Drawing.Color.Transparent;
+            this.btnp1.UncheckedState.InnerColor = System.Drawing.Color.Transparent;
+            // 
+            // btnp2
+            // 
+            this.btnp2.AutoSize = true;
+            this.btnp2.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.btnp2.CheckedState.BorderThickness = 0;
+            this.btnp2.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.btnp2.CheckedState.InnerColor = System.Drawing.Color.White;
+            this.btnp2.CheckedState.InnerOffset = -4;
+            this.btnp2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnp2.Location = new System.Drawing.Point(596, 447);
+            this.btnp2.Name = "btnp2";
+            this.btnp2.Size = new System.Drawing.Size(77, 29);
+            this.btnp2.TabIndex = 10;
+            this.btnp2.Text = "Vote";
+            this.btnp2.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.btnp2.UncheckedState.BorderThickness = 2;
+            this.btnp2.UncheckedState.FillColor = System.Drawing.Color.Transparent;
+            this.btnp2.UncheckedState.InnerColor = System.Drawing.Color.Transparent;
+            // 
+            // studentDashBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 585);
+            this.Controls.Add(this.btnp2);
+            this.Controls.Add(this.btnp1);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.pParty1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "studentDashBoard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "studentDashBoard";
+            this.Load += new System.EventHandler(this.studentDashBoard_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).EndInit();
+            this.pParty1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbParty1)).EndInit();
+            this.panel5.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbParty2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox1;
+        private System.Windows.Forms.Label lblTitle;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox2;
+        private Guna.UI2.WinForms.Guna2Button btnConfirm;
+        private Guna.UI2.WinForms.Guna2Button btnCancel;
+        private System.Windows.Forms.Panel pParty1;
+        private System.Windows.Forms.Panel panel2;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox pbParty1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox pbParty2;
+        private System.Windows.Forms.Label lblparty1;
+        private System.Windows.Forms.Label lblparty2;
+        private Guna.UI2.WinForms.Guna2RadioButton btnp1;
+        private Guna.UI2.WinForms.Guna2RadioButton btnp2;
+    }
+}
---
+++ studentDashBoard.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sisc
+{
+    public partial class studentDashBoard : Form
+    {
+        string CmsID=null;
+        public studentDashBoard()
+        {
+            InitializeComponent();
+
+        }
+       //  Result r = new Result();
+        public studentDashBoard(string id)
+        {
+            InitializeComponent();
+            CmsID = id;
+          //  Result.name = "ss";
+
+        }
+            SiscdbDataContext con = new SiscdbDataContext();
+        int P1count = 0,P2count=0;
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pParty1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+ 
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Are you sure to vote for this?", "Confirm Vote", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Student s = con.Students.SingleOrDefault(x=>x.CMS == CmsID);
+                s.casting = "1";
+                con.SubmitChanges();
+                if(btnp1.Checked==true)
+                {
+                    P1count++;
+                    Reslt party = con.Reslts.Single(x => x.Id == 7);
+                    party.Votes = party.Votes + 1;
+                    con.SubmitChanges();
+
+                }
+                if (btnp2.Checked == true)
+                {
+                    P2count++;
+                    Reslt party = con.Reslts.Single(x => x.Id == 8);
+                    party.Votes = party.Votes + 1;
+                    con.SubmitChanges();
+                }
+                Result.res[0] = P1count;
+                Result.res[1] = P2count;
+                MessageBox.Show("confrm msg is displayed");
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            frmLogin l = new frmLogin();
+            this.Hide();
+            l.ShowDialog();
+               
+
+        }
+
+        private void studentDashBoard_Load(object sender, EventArgs e)
+        {
+            //var q = from p in con.Parties
+            //      select p;
+           Party p = con.Parties.SingleOrDefault(x => x.Id == 7);
+            //pbParty1.Image =  p.Partylogo
+
+            byte[] buffer = p.Partylogo.ToArray();
+            MemoryStream memStream = new MemoryStream();
+            memStream.Write(buffer, 0, buffer.Length);
+
+            this.pbParty1.Image = Image.FromStream(memStream);
+            this.lblparty1.Text = p.PartyName;
+
+            Party p2 = con.Parties.SingleOrDefault(x => x.Id == 8);
+            byte[] buffer2 = p2.Partylogo.ToArray();
+            MemoryStream memStream2 = new MemoryStream();
+            memStream2.Write(buffer2, 0, buffer2.Length);
+
+            this.pbParty2.Image = Image.FromStream(memStream2);
+            this.lblparty2.Text = p2.PartyName;
+
+            // Dim sqlEmpmaster = con.Parties(GlobalVariable.CurrentCompany, 11111).FirstOrDefault;
+            //  pbParty1.Image = byteArrayToImage(sqlEmpmaster.EMP_PT.ToArray());
+
+            //  Image byteArrayToImage(byte[] byteArrayIn)
+            //{
+            //  MemoryStream stream = new MemoryStream(byteArrayIn);
+            // Image ret = Image.FromStream(stream);
+            //return ret;
+            //}
+        }
+
+        
+
+       
+       
+    }
+}
