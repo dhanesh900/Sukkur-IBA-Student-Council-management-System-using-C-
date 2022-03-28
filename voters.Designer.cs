e041794b544e6e655fddec3781615e140f518b0a
Dhanesh kumar <dhanesh.bcss19@iba-suk.edu.pk>
2022-03-28 16:29:06 +0500

    creating pooling system where student can cast vote

---
+++ voters.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace Sisc
+{
+    partial class voters
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.guna2CirclePictureBox2 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.gvVoter = new System.Windows.Forms.DataGridView();
+            this.btnApproved = new Guna.UI2.WinForms.Guna2Button();
+            this.btnBack = new Guna.UI2.WinForms.Guna2Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvVoter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(46)))), ((int)(((byte)(146)))));
+            this.panel1.Controls.Add(this.guna2CirclePictureBox2);
+            this.panel1.Controls.Add(this.lblTitle);
+            this.panel1.Controls.Add(this.guna2CirclePictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(807, 88);
+            this.panel1.TabIndex = 0;
+            // 
+            // guna2CirclePictureBox2
+            // 
+            this.guna2CirclePictureBox2.Image = global::Sisc.Properties.Resources.logo_white;
+            this.guna2CirclePictureBox2.ImageRotate = 0F;
+            this.guna2CirclePictureBox2.Location = new System.Drawing.Point(716, 12);
+            this.guna2CirclePictureBox2.Name = "guna2CirclePictureBox2";
+            this.guna2CirclePictureBox2.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CirclePictureBox2.ShadowDecoration.Parent = this.guna2CirclePictureBox2;
+            this.guna2CirclePictureBox2.Size = new System.Drawing.Size(64, 64);
+            this.guna2CirclePictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2CirclePictureBox2.TabIndex = 2;
+            this.guna2CirclePictureBox2.TabStop = false;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(223, 28);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(328, 30);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Student Eligible For Vote";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // guna2CirclePictureBox1
+            // 
+            this.guna2CirclePictureBox1.Image = global::Sisc.Properties.Resources.logo_white;
+            this.guna2CirclePictureBox1.ImageRotate = 0F;
+            this.guna2CirclePictureBox1.Location = new System.Drawing.Point(23, 12);
+            this.guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
+            this.guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CirclePictureBox1.ShadowDecoration.Parent = this.guna2CirclePictureBox1;
+            this.guna2CirclePictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.guna2CirclePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2CirclePictureBox1.TabIndex = 0;
+            this.guna2CirclePictureBox1.TabStop = false;
+            // 
+            // gvVoter
+            // 
+            this.gvVoter.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvVoter.Location = new System.Drawing.Point(0, 82);
+            this.gvVoter.Name = "gvVoter";
+            this.gvVoter.Size = new System.Drawing.Size(807, 407);
+            this.gvVoter.TabIndex = 1;
+            // 
+            // btnApproved
+            // 
+            this.btnApproved.BorderRadius = 20;
+            this.btnApproved.CheckedState.Parent = this.btnApproved;
+            this.btnApproved.CustomImages.Parent = this.btnApproved;
+            this.btnApproved.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnApproved.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnApproved.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnApproved.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnApproved.DisabledState.Parent = this.btnApproved;
+            this.btnApproved.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnApproved.ForeColor = System.Drawing.Color.White;
+            this.btnApproved.HoverState.Parent = this.btnApproved;
+            this.btnApproved.Location = new System.Drawing.Point(641, 517);
+            this.btnApproved.Name = "btnApproved";
+            this.btnApproved.ShadowDecoration.Parent = this.btnApproved;
+            this.btnApproved.Size = new System.Drawing.Size(139, 34);
+            this.btnApproved.TabIndex = 2;
+            this.btnApproved.Text = "Approved";
+            this.btnApproved.Click += new System.EventHandler(this.btnApproved_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.BorderRadius = 20;
+            this.btnBack.CheckedState.Parent = this.btnBack;
+            this.btnBack.CustomImages.Parent = this.btnBack;
+            this.btnBack.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnBack.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnBack.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnBack.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnBack.DisabledState.Parent = this.btnBack;
+            this.btnBack.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnBack.ForeColor = System.Drawing.Color.White;
+            this.btnBack.HoverState.Parent = this.btnBack;
+            this.btnBack.Location = new System.Drawing.Point(496, 517);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.ShadowDecoration.Parent = this.btnBack;
+            this.btnBack.Size = new System.Drawing.Size(139, 34);
+            this.btnBack.TabIndex = 3;
+            this.btnBack.Text = "back";
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // voters
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 571);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnApproved);
+            this.Controls.Add(this.gvVoter);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "voters";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "voters";
+            this.Load += new System.EventHandler(this.voters_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvVoter)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox1;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox2;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.DataGridView gvVoter;
+        private Guna.UI2.WinForms.Guna2Button btnApproved;
+        private Guna.UI2.WinForms.Guna2Button btnBack;
+    }
+}
---
+++ voters.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sisc
+{
+    public partial class voters : Form
+    {
+        public voters()
+        {
+            InitializeComponent();
+        }
+
+        private void btnApproved_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            Cdc back = new Cdc();
+            this.Hide();
+            back.ShowDialog();
+        }
+        SiscdbDataContext con = new SiscdbDataContext();
+
+        private void voters_Load(object sender, EventArgs e)
+        {
+            var query = from s in con.Students select s;
+            gvVoter.DataSource = query;
+        }
+    }
+}
